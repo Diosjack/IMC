a1845aab67e87e98b80d88e93f7ed19f854dc71a
Erick <118775502+Diosjack@users.noreply.github.com>
2023-11-28 20:15:31 -0600

    Add files via upload

---
+++ imc.py
@@ -0,0 +1,27 @@
+print (" Bienvenido")
+print ()
+print ("****************************************************************************")
+nom = input ("¿cual es tu nombre? ")
+print ("Hola",nom)
+print ("*****************************************")
+fec = int(input ("¿Cual es su año de nacimiento?"))
+print ("******************************************")
+año = int(input ("¿Cual es el año actual?"))
+print ("******************************************")
+pe = float(input ("¿Cual es su peso?"))
+print ("***********************************************")
+alt = float(input ("¿ Cual es su estatura?"))
+print ("*********************************************************************")
+print ()
+print ("Hola",nom )
+print ("Esta relizando este prueba de IMC en el año ",año)
+edad = año - fec
+print ("su edad es",edad)
+IMC = pe/alt**2
+print ("Su IMC es de",IMC)
+
+
+
+
+
+
